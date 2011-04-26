f290d14dd999fcc0bfd3ede4a6f6f60002bcab1f
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2011-04-26 13:54:52 +0700

    basename. i forgot it damn

--- fetchdata.php
+++ fetchdata.php
@@ -81,7 +81,7 @@ class CrewListLoader {
 		}
 		foreach ($this->xp->query('.//td[@width=52]/img/@src', $node) as $k => $v) {
 			$stages[$k]->song = new Song($v->nodeValue);
-			$stages[$k]->pattern = $v->nodeValue;
+			$stages[$k]->pattern = basename($v->nodeValue, '.png');
 		}
 		$categories = array('fade', 'line', 'scroll');
 		foreach (array_map('trim', explode(':', trim($this->xp->query('.//td[@width=170]/span/text()', $node)->item(0)->nodeValue))) as $stage => $effector) {
