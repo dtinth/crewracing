7a678d15296138d44f6f5c07599b02984642c7c0
Thai Pangsakulyanont <org.yi.dttvb@gmail.com>
2011-04-27 10:42:20 +0700

    allow fetching of all pages. from page 3 only crews with courses are fetched to save bandwidth

--- fetchdata.php
+++ fetchdata.php
@@ -1,5 +1,6 @@
 <?php
 
+
 class Store {
 	function __construct($data = array()) {
 		foreach ($data as $k => $v) {
@@ -58,6 +59,10 @@ class CrewListLoader {
 	function crewFromElement($node) {
 		$crew = new Crew(array(
 			'rank'          => intval($this->xp->query('./td[@width=70]/span/text()', $node)->item(0)->nodeValue),
+			'previousRanks' => array(
+				intval($this->xp->query('./td[@width=320]/table//tr[1]/td[2]/span[2]/text()', $node)->item(0)->nodeValue),
+				intval($this->xp->query('./td[@width=320]/table//tr[1]/td[4]/span[2]/text()', $node)->item(0)->nodeValue),
+			),
 			'emblemPlate'   => new EmblemPlate($this->xp->query('./td[@width=61]/table/@background', $node)->item(0)->nodeValue),
 			'emblemPattern' => new EmblemPattern($this->xp->query('./td[@width=61]/table//img/@src', $node)->item(0)->nodeValue),
 			'name'          => trim($this->xp->query('./td[@width=170]/span/text()', $node)->item(0)->nodeValue),
@@ -104,7 +109,7 @@ class CrewListLoader {
 		));
 	}
 
-	function load(&$out) {
+	function load(&$out, $coursesOnly = false) {
 		
 		$this->data = file_get_contents($this->url);
 		$this->doc = new DOMDocument;
@@ -113,10 +118,14 @@ class CrewListLoader {
 		$query = '//table[@width=725]//table[@width=661]//table[@width=661][.//table[@width=661]]//tr[@onmouseover]';
 		$this->xp = new DOMXPath($this->doc);
 		
-		foreach ($this->xp->query($query) as $node) {
+		$list = $this->xp->query($query);
+		foreach ($list as $node) {
 			$crew = $this->crewFromElement($node);
-			$out[] = $crew;
+			if (!$coursesOnly || $crew->course) {
+				$out[] = $crew;
+			}
 		}
+		return $list->length;
 
 	}
 
@@ -124,16 +133,20 @@ class CrewListLoader {
 
 $crews = array();
 
-$urls = array(
-	//'testdata.txt',
-	'http://www.djmaxcrew.com/crewrace/crewrace_ing.asp?page=1',
-	'http://www.djmaxcrew.com/crewrace/crewrace_ing.asp?page=2',
-	'http://www.djmaxcrew.com/crewrace/crewrace_ing.asp?page=3',
-);
+//$prefix = './test-data/testdata.';
+$prefix = 'http://www.djmaxcrew.com/crewrace/crewrace_ing.asp?page=';
 
-foreach ($urls as $url) {
+$stderr = @fopen('php://stderr', 'w');
+for ($page = 1; $page < 50; $page ++) {
+	$url = $prefix . $page;
+	if ($stderr) {
+		fprintf($stderr, "Fetching: %s\n", $url);
+	}
 	$loader = new CrewListLoader($url);
-	$loader->load($crews);
+	if ($loader->load($crews, $page > 3) == 0) {
+		fprintf($stderr, "No more pages!\n");
+		break;
+	}
 }
 
 echo json_encode(array(
